73e50487b6004d1c3c7c48d38f9d7fbaecc59bb3
Tony Wei <jscswch@hotmail.com>
2025-05-28 15:42:05 -0700

    add NPE

--- src/constants.js
+++ src/constants.js
@@ -2,7 +2,7 @@ export const scaleFactor = 4;
 export const playerScaleFactor = 6;
 
 export const dialogueData = {
-    resume: `This is my <a href="https://drive.google.com/file/d/1xDLzkHM5d_oxY9X3HzA6eIrxtgKkdo2I/view?usp=sharing" target="_blank">Resume</a>.
+    resume: `This is my <a href="https://drive.google.com/file/d/11rCaRkBZ-VLHzKPx4U9bNly3HI1rmtXx/view?usp=sharing" target="_blank">Resume</a>.
              Let's also stay connected on <a href="http://www.linkedin.com/in/chenhaotonywei" target="_blank">LinkedIn</a>.`,
 
     fridge: `What's inside the fridge? Only my favorite foods: lobster and lamb chops.`,
@@ -13,9 +13,11 @@ export const dialogueData = {
 
     pc: `I publish all the games I create on my <a href="https://frigidough.itch.io" target="_blank">itch.io</a> page. 
     Feel free to take a look! Contact me at <a href = "mailto:weichenhao2020@gmail.com">weichenhao2020@gmail.com</a > if you have any job opportunities!`,
-
-    JJDA: `JoJo's Doom Adventure is my latest project, a first-person shooter made in Unreal Engine 5, inspired by classic DOOM with some JoJo's Bizarre Adventure references. 
+  
+    JJDA: `JoJo's Doom Adventure is my biggest project so far, a first-person shooter made in Unreal Engine 5, inspired by classic DOOM with some JoJo's Bizarre Adventure references. 
     For a detailed breakdown and highlights, click <a href="jojos-doom-adventure.html" target="_blank">here</a>.`,
+
+    NPE: `Nine Point Eight is my favorite creation so far. It's a platformer about gravity shift. I strongly encourage you to <a href="https://frigidough.itch.io/nine-point-eight" target="_blank">try</a> it out, it's really fun! Or click <a href="nine-point-eight.html" target="_blank">here</a> for a breakdown.`,
 
     DOD: `Dungeon of the Dead is a game I created when I first started learning Unreal Engine 5. It's a walking simulator with puzzles and occasional jump scares. 
     Click <a href="dungeon-of-dead.html" target="_blank">here</a> for more details.`,
